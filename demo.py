4f6c01d8cccd383f7d88c3461a4956c332200622
ryuming <huynhtrongnghia10012000@gmail.com>
2025-07-22 15:34:51 +0700

    remove sensitive data

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 import asyncio
 from dotenv import load_dotenv
 load_dotenv()
+import os
 
 from langchain_mistralai import ChatMistralAI
 from chat import ChatLangchain
-from browser_use import Agent, Controller, BrowserSession, BrowserProfile
+from browser_use import Agent, Controller, BrowserSession, BrowserProfile, AgentHistoryList
 from browser_use.llm import ChatOpenAI, ChatGoogle
 import handler
 
@@ -39,38 +40,57 @@ async def main():
     # print(f'Give me answer for surver question 2: "How do you feel about the current state of the world?"')
     # answer = input('Your answer: ')
     # if not answer:
-    #     answer = ""
+    answer = "Test"
 
-    webchatURL = 'https://cx.filum.ai/conversation-channels/webchat/3184'
-    workspace = 'thien+dance'
+    targetURL = 'https://app.filum.asia/ai-automation?tab=ai-settings'
+    workspace = 'Test AI Agent'
+    domain_url = 'https://app.filum.asia'
+    username = os.environ.get('FILUM_STAGING_USERNAME')
+    password = os.environ.get('FILUM_STAGING_PASSWORD')
+    sensitive_data = {
+         'https://*.filum.asia': {
+        'x_username': username,
+        'x_password': password,
+    },
+    }
 
-    for i in range(2):
+    for i in range(1):
         agent = Agent(
             task=(
-                'Important : I am a UI Automation Tester sending message in vietnamese to webchat of Filum company by using Playwright. to verify response \n'
-                'Remember: wait for response message before sending the next message\n'
-                '1. Open browser, check if login is required, if yes, then login to "cx.filum.ai" with username "nghia.huynh@filumai.com" and password "Nghiahuynh832@", if not, then continue to step 2\n'
+            # 'Important : I am a UI Automation Tester validating the survey feature of Filum company by using Playwright. \n'
+            # 'Remember: if instruction require choose/pick/select randomly, they must be ramdom, read entire answers from survey before deciding. Please record your answer for each question in survey and show me when your task is done.\n'
+            # '1. Open browser, from the curent tab, navigate to website https://survey.filum.ai/l/txbxef \n'
+            # '2. In "Câu hỏi 1", click randomly into one of five icon in center of the page, then click into "Tiếp tục" button\n'
+            # f'3. In "Câu hỏi 2", fill the text field with "{answer}", wait for 3 seconds and click into "Tiếp tục" button\n'
+            # '4. In "Câu hỏi 3", Choose 2 random answers from the answer list, wait for 3 seconds and click into "Tiếp tục" button\n'
+            # '5. In "Câu hỏi 4", Select one random value for each row, wait for 3 seconds and click into "Next" or "Tiếp tục" button\n'
+            # '6. In "Câu hỏi 5", Fill any random string (about 20 characters) into "Giảng viên" and "Lịch học" text field, Click into "Next" or "Tiếp tục" button\n'
+            # '7. If screen shows "Cảm ơn bạn đã phản hồi", then the task is completed successfully. Test case is passed, please tell me the test case is passed or not and the survey answers set you have recorded.\n'
+
+                'Important : I am a UI Automation Tester configuring AI&Automation setting of Filum company by using Playwright.  \n'
+                'Remember: If element are not visible, then scroll to the element. Please record your steps show me when your task is done.\n'
+                f'1. Open browser, check if login is required, if yes, then login to {domain_url} with x_username and x_password, if not, then continue to step 2\n'
                 f'2. Select workspace in the left of avatar icon, then select {workspace} workspace\n'
-                f'3. Open new tabs navigate to website {webchatURL} \n'
-                '4. Open webchat in bottom right corner, then send a message to Filum webchat, the message is "Hello, this is a test message from Playwright" \n'
-                '5. Wait for response message from Filum webchat, then record the sent message and response message \n'
-                '6. Send message "cho tui xin giá mấy chiếc xe bên mình đi" to Filum webchat" \n'
-                '7. Wait for response message from Filum webchat, then record the sent message and response message \n'
-                '8. Send message "xe cùi bắp mà cũng không có, cáu thật" to Filum webchat\n'
-                '9. Wait for response message from Filum webchat, then record the sent message and response message \n'
-                '10. Click on avatar icon in the top right corner, then click on "Đăng xuất" or "Log out" button to logout\n'
-                '11. Show me the sent messages and response messages when your task is done.\n'
+                f'3. Navigate to website {targetURL} \n'
+                '4. Click into first Agent from the list \n'
+                '5. Open Overview tab, then open Instruction modal \n'
+                '6. Add Instruction with name from "Instruction1" to "Instruction20" and Save changes after each step \n'
+                '7. Click on avatar icon in the top right corner, then click on "Đăng xuất" or "Log out" button to logout\n'
+                '8. Show me the your steps and instruction list you created when your task is done.\n'
             ),
             llm=ChatGoogle(
-                model= 'gemini-2.5-flash'
+                model= 'gemini-2.0-flash-exp'
             ),
+            sensitive_data=sensitive_data,
             controller=controlHandler,
             use_vision=True,
             browser_session=browser_session,
         )
+        #code = AgentHistoryList().save_as_playwright_script(output_path= "generated_scripts\github_search.py")
         history = await agent.run()
         test_result = history.final_result()
         print("\n"+ test_result)
+        print(f'Generated script saved to: {code}')
 
 asyncio.run(main())
 
